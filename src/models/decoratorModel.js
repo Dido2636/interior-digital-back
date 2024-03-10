7474b9d2fab20ba77ccf7887b9583f9609652a25
Do <do@MBP-de-dolores.lan>
2024-03-10 15:50:15 +0100

    Ajout fichier decoratormodel

--- src/models/decoratorModel.js
+++ src/models/decoratorModel.js
@@ -1,25 +1,23 @@
 import { Schema, mongoose } from "mongoose";
-import bcrypt from "bcryptjs"
+import bcrypt from "bcryptjs";
 
 const decoratorSchema = new Schema({
-    name : String,
-    firstname : String,
-    email: { type : String,},
-    password: String,
-})
+  name: String,
+  firstname: String,
+  email: { type: String },
+  password: String,
+});
 
 decoratorSchema.pre("save", async function (next) {
-    if (!this.isModified("password")) return next();
-    try {
-      const hashedPassword = await bcrypt.hash(this.password, 10);
-      this.password = hashedPassword;
-      next();
-    } catch (error) {
-      next(error);
-    }
-  });
-
-
+  if (!this.isModified("password")) return next();
+  try {
+    const hashedPassword = await bcrypt.hash(this.password, 10);
+    this.password = hashedPassword;
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
 
 const Decorator = mongoose.model("Decorator", decoratorSchema);
 
