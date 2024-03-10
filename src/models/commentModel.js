3c5248540977b2f38c04e263fc80d6ec62fe5cde
Do <do@MBP-de-dolores.lan>
2024-03-10 19:07:01 +0100

    Ajout fichier commentModel

--- src/models/commentModel.js
+++ src/models/commentModel.js
@@ -1,11 +1,11 @@
 import { Schema, mongoose } from "mongoose";
 
-const commentSchema = new Schema ({
-    commentaire: String,
-    mediaType:{type: String, enum: ['plan', '3d', 'Devis'],required: true},
-    mediaId: { type: mongoose.Schema.Types.ObjectId, required: true },
-    author: { type: mongoose.Schema.Types.ObjectId, ref:'User', required: true}
-})
+const commentSchema = new Schema({
+  commentaire: String,
+  mediaType: { type: String, enum: ["plan", "3d", "Devis"], required: true },
+  mediaId: { type: mongoose.Schema.Types.ObjectId, required: true },
+  author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+});
 
 const Comment = mongoose.model("Comment", commentSchema);
 
